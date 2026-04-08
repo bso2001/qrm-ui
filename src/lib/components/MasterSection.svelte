adb517ad4515485d59f046df6de95d57975d8331
Bert Olsson <bert.olsson@gmail.com>
2026-04-08 08:11:40 -0400

    Remove spaces inside song title brackets

--- src/lib/components/MasterSection.svelte
+++ src/lib/components/MasterSection.svelte
@@ -27,16 +27,12 @@
 
 <Card title="">
   <div slot="header-left-extra" class="title-group">
-    <h2 class="song-title">SONG <span class="bracket">[</span>
-      <div class="input-sizer" data-value={$songStore.name || 'Untitled'}>
-        <input 
+    <h2 class="song-title">SONG <span class="bracket">[</span><div class="input-sizer" data-value={$songStore.name || 'Untitled'}><input 
             class="name-input highlight" 
             bind:value={$songStore.name} 
             placeholder="Untitled"
             size="1"
-        />
-      </div>
-    <span class="bracket">]</span></h2>
+        /></div><span class="bracket">]</span></h2>
   </div>
 
   <div slot="header-right-extra" class="header-controls">
